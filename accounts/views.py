d6d300cc606b734ee8df609f38941e794a13d475
soonju <poodle0403@likelion.org>
2019-07-31 04:03:49 +0900

    로그인/회원가입 기능

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import Inputform
 
 def home(request): 
@@ -32,3 +33,33 @@ def inputform(request):
 def detail(request):
     input_information = Inputform.objects
     return render(request, 'detail.html', {'input_information' : input_information})
+
+
+
+
+####################################################################################################
+
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+
+def register(request):
+    if request.method == 'POST':     
+        if request.POST['password1'] == request.POST["password2"]:        
+            user = User.objects.create_user(username = request.POST['username'], password = request.POST['password1'])
+            auth.login(request, user)
+            return redirect('home.html')
+    return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, password = password)
+        if user is not None: 
+            auth.login(request, user)
+            return redirect('home')
+        else: 
+            return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
+    return render(request, 'login.html')
--- matzip/urls.py
+++ matzip/urls.py
@@ -5,9 +5,9 @@ import accounts.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', accounts.views.home, name="home"),
-    path('login', accounts.views.login, name="login"),
-    path('register', accounts.views.register, name="register"),
     path('input', accounts.views.input, name="input"),
     path('inputform', accounts.views.inputform, name="inputform"),
     path('detail', accounts.views.detail, name="detail"),
+    path('login', accounts.views.login, name="login"),
+    path('register', accounts.views.register, name="register"),
 ]
