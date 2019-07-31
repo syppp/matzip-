503657a4df0682703f1315378653ad854f08a612
syp <p1645@naver.com>
2019-07-31 17:26:24 +0900

    board.html

--- matzip/urls.py
+++ matzip/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 import accounts.views
+import product.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('detail', accounts.views.detail, name="detail"),
     path('login', accounts.views.login, name="login"),
     path('register', accounts.views.register, name="register"),
+    path('board', product.views.board, name="board"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import Inputform
 # Create your views here.
+def board(request):
+    return render(request, 'board.html')
