90acb36374a553bbfe1d730702dfea3370b08d5c
superbunny38 <superbunny38@naver.com>
2019-08-01 03:17:03 +0900

    사진 업로드가 안돼,,

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Inputform(models.Model):
     title = models.TextField()
     product_name = models.TextField()
@@ -10,4 +11,6 @@ class Inputform(models.Model):
     account = models.TextField()
     date_limit = models.DateTimeField('date published')
     explanation = models.CharField(max_length=300)
-    photo = models.FileField()
+    photo = models.ImageField(blank=True)
+    #photo = models.FileField()
+
