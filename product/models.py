ee2c96ac77900c43a2a2dca2d484566b424fd961
syp <p1645@naver.com>
2019-07-31 20:52:03 +0900

    board3

--- product/models.py
+++ product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from accounts.models import Inputform
 # Create your models here.
+class Comment(models.Model):   
+    comment_date = models.DateTimeField(auto_now_add=True)
+    comment_user = models.TextField(max_length=20)
+    comment_thumbnail_url = models.TextField(max_length=300) 
+    comment_textfield = models.TextField()
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from accounts.models import Inputform
+from .models import Comment
 # Create your views here.
 def board(request):
     return render(request, 'board.html')
@@ -9,5 +10,9 @@ def detail(request):
     input_information = Inputform.objects
     return render(request, 'board.html', {'input_information' : input_information})
 
-
+#댓글
+def comments(request):
+    
+    comments=Comment.objects
+    return render(request, 'board.html', {'comments':comments}) 
     
